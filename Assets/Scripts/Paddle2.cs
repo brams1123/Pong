02697fb46bca893e6b57d7d7c1de6d2ffbc9c1fa
brams1123 <bramsosifo@gmail.com>
2017-07-10 14:46:33 -0400

    first commit

---
+++ Assets/Scripts/Paddle2.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle2 : MonoBehaviour {
+
+    public float padSpeed = 1;
+    public Vector3 playPos;
+	
+	// Update is called once per frame
+	void Update () {
+
+        float yPos = transform.position.y + (Input.GetAxis("Horizontal") * padSpeed);
+        playPos = new Vector3(23, Mathf.Clamp(yPos, -11, 11), 0);
+        transform.position = playPos;
+    }
+}
